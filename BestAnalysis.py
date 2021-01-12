876dd677e6645892d945fa0dcbad9e16680bbdbf
dseop <ehdtjq4650@gmail.com>
2021-01-13 01:13:50 +0900

    rank

--- BestAnalysis.py
+++ BestAnalysis.py
@@ -6,26 +6,50 @@ import time
 from datetime import datetime as dt
 
 import crawling as cr
-from get_book_info import gbi_yes
+from get_book_info import yes24
 # from keyword_analysis import get_tags_okt
 
 def yes24() :
     # URL request possible
     # http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=1
     url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber="
-    for i in [1,2,3,4] :
+    
+    row_list = []
+    for i in [1, 2, 3, 4] :
         tmp_url = url+('%s' %i)
         par_url = cr.makepar(tmp_url)
-        pritn(par_url)
+        for good_info in par_url.find_all('td', 'goodsTxtInfo') :
+            
+            code_url = good_info.find('a', href=True)['href']
+            code = code_url.split('/')[-1] # id
+            title = good_info.find_all('p')[0].get_text(' ',strip=True).replace('[도서] ', '')
+            
+            aupu_list = []
+            for aupu in good_info.find('div', 'aupu').find_all('a') : # author list
+                aupu_list.append(aupu.get_text())
+            if len(aupu_list) > 1 :
+                au = aupu_list[0] # aupu_list[0:-1]
+                pu = aupu_list[-1]
+            else :
+                au = 'need check'
+                pu = 'need check'
+            
+            date = good_info.find('div', 'aupu').get_text('',strip=True).split('| ')[-1]
+            price = int(good_info.find_all('p')[1].get_text().split('원')[0].replace(',',''))
 
-def kyobo() :
+            row = code,title,au,pu,date,price,"http://www.yes24.com"+code_url
+            print(row)
+            row_list.append(row)
+    print(len(row_list)) # 1day 320 / 1year 116800 / 2year 233600 (이정도 모으기 전에 그만두지 않을까?)
+    return 0
+
+def kyobo() : return 0
     # URL request impossible(responsive web)
     # 경제경영 전체로 볼 때는 150위까지 제공, 반응형 → use pyautogui
     # 세부 분야로 들어가면 전체 도서 제공, url request possible
     # http://www.kyobobook.co.kr/categoryRenewal/categoryMain.laf?linkClass=1323&mallGb=KOR&orderClick=JAR
-def aladin() :
-    # URL request possible
+
+def aladin() : return 0
     # https://www.aladin.co.kr/shop/common/wbest.aspx?BestType=Bestseller&BranchType=1&CID=170&page=1&cnt=1000&SortOrder=1
 
 yes24()
-
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber="
-for i in [1,2,3,4] :
+for i in [1] :
     tmp_url = url+('%s' %i)
     print(tmp_url)
+
+
+aupu_text = """마우로 기옌 저/ 우진하 역
+
+
+                    | 리더스북 | 2020년 10월"""
