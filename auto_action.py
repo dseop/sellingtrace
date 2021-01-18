efa4b7986c89edbf1e615821f3c2a0dc5f18405b
dseop <ehdtjq4650@gmail.com>
2021-01-18 13:44:33 +0900

    other category

--- auto_action.py
+++ auto_action.py
@@ -1,8 +1,7 @@
 # import sell_tracing
 import best_analysis
 
-# url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=" # 경제경영
-# best_analysis.yes24_economy(url)
-
-url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001019&sumgb=06&FetchSize=80&PageNumber=" # 인문
-best_analysis.yes24_humanities(url)
+# url = "economy", "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=" # 경제경영
+# url = "humanities", "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001019&sumgb=06&FetchSize=80&PageNumber=" # 인문
+url = "ebiz", "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025011&sumgb=06&FetchSize=80&PageNumber=" # 인터넷 비즈니스
+best_analysis.yes24(url)
--- best_analysis.py
+++ best_analysis.py
@@ -6,65 +6,20 @@ import connect_sheet as cs
 
 # from keyword_analysis import get_tags_okt
 
-def yes24_economy(url) :
+def yes24(url) :
     
     # URL request possible
     # http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=1
+
     doc = cs.open_sheet()
-    worksheet = doc.worksheet('RANK_YES_economy') # select sheet
+    worksheet = doc.worksheet('RANK_YES_'+url[0]) # select sheet
 
     print('get daily BEST 320...')
 
     rank = 0
     row_list = []
     for i in [1, 2, 3, 4, 5] : # 400위까지
-        tmp_url = url+('%s' %i)
-        par_url = cr.makepar(tmp_url)
-        for good_info in par_url.find_all('td', 'goodsTxtInfo') :
-            
-            rank = rank+1
-            code_url = good_info.find('a', href=True)['href']
-            code = int(code_url.split('/')[-1]) # id
-            title = good_info.find_all('p')[0].get_text(' ',strip=True).replace('[도서] ', '')
-            
-            aupu_list = []
-            for aupu in good_info.find('div', 'aupu').find_all('a') : # author list
-                aupu_list.append(aupu.get_text())
-            if len(aupu_list) > 1 :
-                au = aupu_list[0] # aupu_list[0:-1]
-                pu = aupu_list[-1]
-            else :
-                au = 'need check'
-                pu = 'need check'
-            
-            date = good_info.find('div', 'aupu').get_text('',strip=True).split('| ')[-1]
-            price = int(good_info.find_all('p')[1].get_text().split('원')[0].replace(',',''))
-
-            info_row = rank,code,title,au,pu,date,price,cr.date,"http://www.yes24.com"+code_url    
-            print(info_row)
-            # worksheet.append_row(info_row)
-            row_list.append(info_row)
-    worksheet.append_rows(row_list)
-
-    print(len(row_list), ' data insert complete!') # 1day 320 / 1year 116800 / 2year 233600 (이정도 모으기 전에 그만두지 않을까?)
-    # 어쩄거나 데이터를 정리하려면, DB를 만드는 게 좋다.
-    # 320위에서 벗어나버리면 점점 안 보게 될 것. 내가 선택적으로 그 때 그 때 데이터를 보는 게 낫다.
-    # 일단은 스프레드시트? 피봇테이블로 감당할 수 있을까..?
-    return 0
-
-def yes24_humanities(url) :
-    
-    # URL request possible
-    # http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=1
-    doc = cs.open_sheet()
-    worksheet = doc.worksheet('RANK_YES_humanities') # select sheet
-
-    print('get daily BEST 320...')
-
-    rank = 0
-    row_list = []
-    for i in [1, 2, 3, 4, 5] : # 400위까지
-        tmp_url = url+('%s' %i)
+        tmp_url = url[1]+('%s' %i)
         par_url = cr.makepar(tmp_url)
         for good_info in par_url.find_all('td', 'goodsTxtInfo') :
             
