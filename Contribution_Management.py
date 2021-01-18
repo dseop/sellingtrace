538cc9b7252f8031d99ca4c8b1a2795e3ab2df37
dseop <ehdtjq4650@gmail.com>
2021-01-18 09:43:52 +0900

    humanities category update

---
+++ Contribution_Management.py
@@ -0,0 +1,3 @@
+# 투고 원고를 자동으로 정리해주는 프로그램
+# not use requests, chromedriver
+# have no authority from home
--- auto_action.py
+++ auto_action.py
@@ -1,4 +1,8 @@
-import sell_tracing
+# import sell_tracing
 import best_analysis
 
-best_analysis.yes24()
+# url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=" # 경제경영
+# best_analysis.yes24_economy(url)
+
+url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001019&sumgb=06&FetchSize=80&PageNumber=" # 인문
+best_analysis.yes24_humanities(url)
