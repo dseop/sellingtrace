8b177773dc1a9d19f285555f92d63da070104cc2
dseop <ehdtjq4650@gmail.com>
2021-01-20 01:11:37 +0900

    db update

--- best_analysis.py
+++ best_analysis.py
@@ -1,27 +1,29 @@
 # https://www.notion.so/7cadd2f2f5b64f208fa9cfbde4cfcf30#3c22dc1cd4b74b81a413197381dbec04
 # rank analysis
 
-# import sqlite3
+import sqlite3
 import crawling as cr
 import connect_sheet as cs
 
-# con = sqlite3.connect("rank_DB.db")
-# c = con.cursor()
-
 def yes24(url) :
+    # DB connect    
+    con = sqlite3.connect("rank_DB.db")
+    c = con.cursor()
     
-    # URL request possible
-    # http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=06&FetchSize=80&PageNumber=1
-
+    # Spreadsheet connect
     doc = cs.open_sheet()
     worksheet = doc.worksheet('RANK_YES_'+url[0]) # select sheet
 
+    # start
     print('get daily BEST 320...')
 
     rank = 0
     row_list = []
-    # db_list = [] # about DB
-    for i in [1, 2, 3, 4, 5] : # 400위까지
+    rank_db_list = [] # make DB list
+    book_db_list = []
+
+    # page 1~5, get bestseller list
+    for i in [1] : # , 2, 3, 4, 5] : # 400위까지
         tmp_url = url[1]+('%s' %i)
         par_url = cr.makepar(tmp_url)
         for good_info in par_url.find_all('td', 'goodsTxtInfo') :
@@ -44,22 +46,38 @@ def yes24(url) :
             date = good_info.find('div', 'aupu').get_text('',strip=True).split('| ')[-1]
             price = int(good_info.find_all('p')[1].get_text().split('원')[0].replace(',',''))
 
+            # into Spreadsheet data
             info_row = rank,code,title,au,pu,date,price,cr.date,"http://www.yes24.com"+code_url    
             row_list.append(info_row)
 
-            # db_row = rank,code,cr.date # about DB
-            # db_list.append(db_row) # about DB
+            # into DB data
+            rank_db_row = rank, code, cr.date
+            rank_db_list.append(rank_db_row)
+
+            book_db_row = code, title, au, pu, date, price
+            book_db_list.append(book_db_row)
             
         print(i, "page complete!")
-    worksheet.append_rows(row_list)
-    print(db_list) # about DB
+    # insert into Spreadsheet
+    # worksheet.append_rows(row_list)
 
-    # c.executemany("INSERT INTO rank_table VALUES (?,?,?)", db_list) # about DB
-    # con.commit() # about DB
-    # con.close() # about DB
+    # insert into rank_DB
+    # print(rank_db_list) # check DB
+    # c.execute("CREATE TABLE rank_table_"+url[0]+"('rank' int, code int, today text)")
+    # sql_insert_rank = "INSERT INTO rank_table_"+url[0]+" VALUES (?,?,?)"
+    # c.executemany(sql_insert_rank, rank_db_list)
+
+    print(book_db_list)
+    # insert into book_DB
+    # 중복처리 해결 필요 #
+    # c.execute("CREATE TABLE book_table('code' int, 'title' text, 'au' text, 'pu' text, 'date' text, 'price' text)")
+    # c.executemany("INSERT INTO book_table VALUES (?,?,?,?,?,?)", book_db_list)
+    
+    #con.commit()
+    con.close()
 
     print(len(row_list), ' data insert complete! - ', url[0])
-    # 어쩄거나 데이터를 정리하려면, DB를 만드는 게 좋다.
+    
     # 320위에서 벗어나버리면 점점 안 보게 될 것. 내가 선택적으로 그 때 그 때 데이터를 보는 게 낫다.
     # 일단은 스프레드시트? 피봇테이블로 감당할 수 있을까..?
     return 0
--- rank_DB.py
+++ rank_DB.py
@@ -1,14 +1,55 @@
-con, c = open_db()
-
-c.execute("CREATE TABLE rank_table('rank' int, code int, today text)")
-
-c.execute("INSERT INTO test VALUES('title1', 10, 'no data', 'auth1')")
-c.execute("SELECT * FROM test")
-c.fetchone()
-c.fetchall()
-
 import sqlite3
+import test_db_list as tdl
+
 con = sqlite3.connect("rank_DB.db")
 c = con.cursor()
-c.execute("SELECT * FROM rank_table")
-c.fetchall()
+
+# c.execute("CREATE TABLE rank_table('rank' int, code int, today text)")
+# c.execute("INSERT INTO test VALUES('title1', 10, 'no data', 'auth1')")
+
+# c.execute("SELECT * FROM test")
+# c.fetchall()
+
+c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+print(c.fetchall())
+
+def check_rte() :
+    c.execute("SELECT * FROM rank_table_economy")
+    print(c.fetchall())
+
+# print("no rank data table...")
+# check_rte()
+
+sql_insert_rank = "INSERT INTO rank_table_economy VALUES (?,?,?)"
+c.executemany(sql_insert_rank, tdl.rdl_e)
+
+# print("now? rank data inserted")
+# check_rte()
+
+def check_bt() :
+    c.execute("SELECT * FROM book_table")
+    print(c.fetchall())
+
+# c.execute("CREATE TABLE book_table('code' int, 'title' text, 'au' text, 'pu' text, 'date' text, 'price' text)")
+# c.execute("""CREATE TABLE book_table(
+#     'code' int PRIMARY KEY, 'title' text, 'au' text, 'pu' text, 'date' text, 'price' text
+# )""")
+
+sql_insert_book = "INSERT INTO book_table VALUES (?,?,?,?,?,?)"
+c.executemany(sql_insert_book, tdl.bdl_e)
+
+sql = """INSERT INTO book_table VALUES (?,?,?,?,?,?)
+           ON DUPLICATE KEY UPDATE address = VALUES(address)"""
+
+           ########################### 이거 해결해야 함
+
+print("book data table...")
+check_bt()
+
+# print("now? book data inserted")
+# check_bt()
+
+# c.execute("DROP TABLE book_table")
+
+# con.commit()
+con.close()
