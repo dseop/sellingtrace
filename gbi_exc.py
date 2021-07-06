cb710a30fe3af85b96edf8b9fca570a5ff50e0b2
dseop <ehdtjq4650@gmail.com>
2021-07-07 08:23:34 +0900

    clean

--- gbi_exc.py
+++
@@ -1,5 +0,0 @@
-from get_book_info import yes24 as gy
-
-url_list = 'http://www.yes24.com/Product/Goods/75187118' #.split('\n') #이건 도서 개별 url
-rd = gy(url_list)
-rd.to_csv('getinfo.csv', header=True, index=True, encoding='ms949')
--- sum_excel.py
+++
@@ -1,1 +0,0 @@
-dir_name = 'C:/Users/gilbut/Desktop/하루 만에'
